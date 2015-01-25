c29d06c62cb68c46fd52d8997519e64775e57c32
MattB-GA-Boston <matt.brendzel@generalassemb.ly>
2015-01-25 13:42:02 -0500

    Sets up starting code for students to work from.

--- src/RockPaperScissors.js
+++ src/RockPaperScissors.js
@@ -1,19 +1,35 @@
-// Provided functions - please do not edit these!
-function randomPlay() {
-    var randomNumber = Math.random();
-    if (randomNumber < 0.33) {
-        return "Rock";
-    } else if (randomNumber < 0.66) {
-        return "Paper";
-    } else {
-        return "Scissors";
-    }
-}
-function getPlayerInput() {
+////////////////////////////////////////////////
+/*   Provided Functions - Please Don't Edit   */
+////////////////////////////////////////////////
+function getInput() {
     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
     return prompt();
 }
-///////////////////////////////////////////////////
+function randomPlay() {
+    var randomNumber = Math.random();
+    if (randomNumber < 0.33) {
+        return "rock";
+    } else if (randomNumber < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+////////////////////////////////////////////////
+/*           Write Your Code Below            */
+////////////////////////////////////////////////
+
+function getPlayerMove(move) {
+    // If a move is given, your expression should evaluate to that move.
+    // If move is not specified / is null, your expression should run getInput()
+    return move || getInput();
+}
+
+function getComputerMove(move) {
+    // If a move is given, your expression should evaluate to that move.
+    // If move is not specified / is null, your expression should run randomPlay()
+    return /* Your Expression */;
+}
 
 function getWinner(playerMove,computerMove) {
     // This function should either give us back 'player', 'computer', or 'tie'.
@@ -22,8 +38,18 @@ function getWinner(playerMove,computerMove) {
 
 }
 
-function playGame() {
+function playToFive() {
     console.log("Let's play Rock Paper Scissors");
+    var playerWins = 0;
+    var computerWins = 0;
     // This function should continue to play Rock Paper Scissors until either the player or the computer has won five times.
+
+
+
+    // After each 'round', display some text in the console indicating who played what, who won, and what the current scoreboard looks like.
+    // For example,
+    //  console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
+    //  console.log("The score is currently " + playerWins + " to " + computerWins + "\n");
+    return [playerWins, computerWins];
 }
 
