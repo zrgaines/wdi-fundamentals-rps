582a588d4cd6a20c133317dcbde4049e4887df9f
Matt Brendzel <matt.brendzel@generalassemb.ly>
2015-02-16 20:31:55 -0500

    Update rockpaperscissors_spec.js

--- test/spec/rockpaperscissors_spec.js
+++ test/spec/rockpaperscissors_spec.js
@@ -66,7 +66,7 @@ describe("#getWinner", function() {
 
 describe("#playToFive", function() {
   it ("should end the game once either the player or the computer has won 5 times.", function(){
-    var result = playtoFive();
+    var result = playToFive();
     expect(result[0] == 5 || result[1] == 5).toEqual(true);
   });
 });
